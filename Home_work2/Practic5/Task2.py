cd904e36355897cbaab04216fd4b8dc63247b7e3
Your Name <you@example.com>
2022-12-06 21:26:26 +0300

    New save

---
+++ Home_work2/Practic5/Task2.py
@@ -0,0 +1,22 @@
+# 2. Найдите корни квадратного уравнения Ax² + Bx + C = 0 двумя способами:
+# С помощью математических формул нахождения корней квадратного уравнения
+
+# solve(a, b, c)
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+print(f"Дискриминант D = {round(discr,2)}") 
+
+if discr > 0:
+    x1 = (-b + (discr**0.5)) / (2 * a)
+    x2 = (-b - (discr**0.5)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
